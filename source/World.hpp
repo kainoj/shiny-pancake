1bbe842898c4fd27993f2d4df3d9334ffa74b633
Kentox <vszczerbinski@gmail.com>
2016-12-16 23:21:22 +0100

    Fix

--- source/World.hpp
+++ source/World.hpp
@@ -1,26 +1,32 @@
 #pragma once
 #include "GameObject.hpp"
+#include <memory>
 
-struct World {
-  static World& getInstance() {
-    static World goFactory;
-    return goFactory;
-  }
-  std::vector<std::shared_ptr<GameObject> > toBeAdded;
-  std::vector<std::shared_ptr<GameObject> > objects;
-  int currentObjectID = 0;
-  void spawn(std::shared_ptr<GameObject> go) {
-    go->id = ++currentObjectID;
-    toBeAdded.push_back(go);
-  }
-  void step() {
-    if (!toBeAdded.empty()) {
-      objects.insert(objects.end(), toBeAdded.begin(), toBeAdded.end());
-      toBeAdded.clear();
-    }
-    float dt = 1.0f / 60;
-    for (auto& go : objects) {
-      go->update(dt);
-    }
-  }
+struct World{
+	static World &getInstance()
+	{
+		static World goFactory;
+		return goFactory;
+	}
+	std::vector<std::shared_ptr<GameObject> > toBeAdded;
+	std::vector<std::shared_ptr<GameObject> > objects;
+	int currentObjectID = 0;
+	void spawn(std::shared_ptr<GameObject> go)
+	{
+		go->id = ++currentObjectID;
+		toBeAdded.push_back(go);
+	}
+	void step()
+	{
+		if(!toBeAdded.empty())
+		{
+			objects.insert(objects.end(), toBeAdded.begin(), toBeAdded.end());
+			toBeAdded.clear();
+		}
+		float dt = 1.0f/60;
+		for(auto& go : objects)
+		{
+			go->update(dt);
+		}
+	}
 };
