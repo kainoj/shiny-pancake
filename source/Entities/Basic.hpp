42861a39ba24d6f489eb491a5b5b8fd56ae9a1eb
Kamirus <listopadkamil@gmail.com>
2016-12-17 00:29:12 +0100

    Start working on movement

--- source/Entities/Basic.hpp
+++ source/Entities/Basic.hpp
@@ -1,11 +1,31 @@
 #pragma once
 
+#include <cmath>
+
 struct Basic : public GameObject {
   float hp = 100;
+  float vx = randRange(-10, 10) * 20;
+  float vy = randRange(-10, 10) * 20;
   Basic() : GameObject{"Basic", randRange(1, 2), 0, 0} {}
   virtual void update(float dt) {
-    x = (x + 1) % 500;
-    y = (y + 1) % 500;
+    x = (int) (x + vx * dt);
+    y = (int) (y + vy * dt);
+
+    if (x < 0) {
+      x = 0;
+      vx = -vx;
+    } else if (x > 500) {
+      x = 500;
+      vx = -vx;
+    }
+
+    if (y < 0) {
+      y = 0;
+      vy = -vy;
+    } else if (y > 500) {
+      y = 500;
+      vy = -vy;
+    }
   };
   virtual void updateHP(float dmg) { // dammage taken
   	hp-=dmg;
