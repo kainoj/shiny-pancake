d2d2ca46346cc88717af77f3f8c47629a8e192b2
Radosław Miernik <radekmie@gmail.com>
2016-12-16 19:41:16 +0100

    SVG renderer

--- browser/index.js
+++ browser/index.js
@@ -1,24 +1,54 @@
-const context = createCanvas({width: 300, height: 300});
+const root = document.querySelector('#world');
+const teams = {
+    1: 'red',
+    2: 'blue'
+};
 
 requestAnimationFrame(function frame () {
     requestAnimationFrame(frame);
     draw(data());
 });
 
-function createCanvas (options) {
-    const canvas  = document.querySelector('canvas');
-    const context = canvas.getContext('2d');
-
-    canvas.width  = options.width;
-    canvas.height = options.height;
-
-    return context;
-}
-
 function data () {
     // TODO: C++
+    return [{
+        _id_: 0,
+        posx: 20,
+        posy: 30,
+        type: 'dummy',
+        team: 1
+    }, {
+        _id_: 1,
+        posx: 200,
+        posy: 300,
+        type: 'dummy',
+        team: 2
+    }];
 }
 
 function draw (data) {
-    // TODO: Everything!
+    data.forEach(unit => updateCell(findCell(unit), unit));
+}
+
+function findCell (unit) {
+    const existing = document.getElementById(unit._id_);
+
+    if (existing) {
+        return existing;
+    }
+
+    const fresh = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+
+    fresh.id = unit._id_;
+    fresh.setAttribute('r', 10);
+
+    root.appendChild(fresh);
+
+    return fresh;
+}
+
+function updateCell (cell, unit) {
+    cell.setAttribute('cx', unit.posx);
+    cell.setAttribute('cy', unit.posy);
+    cell.setAttribute('fill', teams[unit.team]);
 }
