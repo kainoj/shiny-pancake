ccce55db0463624d39e07c196fc29a9855f848f0
Kentox <vszczerbinski@gmail.com>
2016-12-17 05:37:14 +0100

    Bullet Garbage collection

--- source/Entities/Bullet.cpp
+++ source/Entities/Bullet.cpp
@@ -4,6 +4,13 @@
 void Bullet::update(float dt) {
   x += velX * dt;
   y += velY * dt;
+  if(x > 1920 || x < 0){
+  	hp = -1;
+  }
+  if(y > 1080 || y < 0)
+  {
+  	hp = -1;
+  }
   World& world = World::getInstance();
   auto unit = world.collidesWithUnit(x, y, team);
   if (unit != nullptr) {
