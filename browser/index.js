aa79511685951e4bff159037e3a1919f68262624
Kamirus <listopadkamil@gmail.com>
2016-12-17 03:16:40 +0100

    Kill Em All (done)

--- browser/index.js
+++ browser/index.js
@@ -64,10 +64,8 @@ function updateCell (cell, unit) {
     cell.setAttribute('transform', `translate(${unit.posx}, ${unit.posy})`);
 
     if (unit.type === 'Spawner') {
-        cell.querySelector('.cell').setAttribute('stroke-dashoffset', ~~(100 - unit._hp_ / 100 * (1000 - 844)));
+        cell.querySelector('.cell').setAttribute('stroke-dashoffset', ~~(1000 - unit._hp_ / 100 * (1000 - 844)));
     } else {
-        cell.querySelector('.cell').setAttribute('stroke-dashoffset', ~~(100 - unit._hp_ / 100 * (1000 - 937)));
+        cell.querySelector('.cell').setAttribute('stroke-dashoffset', ~~(1000 - unit._hp_ / 100 * (1000 - 937)));
     }
-
-    cell.setAttribute('HP', unit._hp_);
 }
