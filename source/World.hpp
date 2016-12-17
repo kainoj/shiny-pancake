1a6ed9c139e62abb0ef9901bf908e04f5cefa31d
Kentox <vszczerbinski@gmail.com>
2016-12-17 01:38:06 +0100

    Terriblr hack but should work

--- source/World.hpp
+++ source/World.hpp
@@ -5,20 +5,22 @@
 class Basic;
 
 struct World {
+  float worldSizeX = 500.0;
+  float worldSizeY = 500.0;
   static World& getInstance() {
     static World goFactory;
     return goFactory;
   }
+  std::queue<unsigned> freeIds;
   std::vector<std::shared_ptr<GameObject> > toBeAdded;
-  std::vector<std::shared_ptr<GameObject> > toBeDeleted;
   std::vector<std::shared_ptr<GameObject> > objects;
   int currentObjectID = 0;
   void spawn(std::shared_ptr<GameObject> go) {
     go->id = ++currentObjectID;
     toBeAdded.push_back(go);
   }
-  void remove(std::shared_ptr<GameObject> go){
-  	toBeDeleted.push_back(go);
+  void remove(std::shared_ptr<GameObject> go) {
+    objects[go->id] = nullptr;  // Terrible hack
   }
   void step() {
     if (!toBeAdded.empty()) {
@@ -27,11 +29,14 @@ struct World {
     }
     float dt = 1.0f / 60;
     for (auto& go : objects) {
-      go->update(dt);
+      if (objects[go->id] != nullptr)  // Terrible hack
+        go->update(dt);
     }
   }
 
-  std::vector<std::shared_ptr<GameObject> > getObjectsInRadius(float x, float y, float radius);
+  std::vector<std::shared_ptr<GameObject> > getObjectsInRadius(float x,
+                                                               float y,
+                                                               float radius);
 
   std::shared_ptr<Basic> getNearestUnit(float x, float y, float radius);
 };
