6f1ada15fe17b38bc8dc04f59c80f4eebf6f7afe
Radosław Miernik <radekmie@gmail.com>
2016-12-17 02:54:27 +0100

    Ranges

--- browser/index.js
+++ browser/index.js
@@ -15,34 +15,53 @@ function findCell (unit) {
     const existing = document.getElementById(unit._id_);
 
     if (existing) {
+        if (unit._hp_ <= 0) {
+            root.removeChild(existing);
+            return null;
+        }
+
         return existing;
     }
 
-    const fresh = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    if (unit._hp_ <= 0) {
+        return null;
+    }
+
+    const fresh  = document.createElementNS('http://www.w3.org/2000/svg', 'g');
+    const range  = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+    const circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
 
     fresh.id = unit._id_;
 
     fresh.classList.add(unit.type);
     fresh.classList.add('team-' + unit.team);
 
+    circle.classList.add('cell');
+
     if (unit.type === 'Basic') {
-        fresh.setAttribute('r', 10);
+        circle.setAttribute('r', 10);
+        range.classList.add('range');
         fresh.classList.add('live');
+        fresh.appendChild(range);
     } else if (unit.type === 'Bullet') {
-        fresh.setAttribute('r', 2);
+        circle.setAttribute('r', 2);
     } else if (unit.type === 'Spawner') {
-        fresh.setAttribute('r', 25);
+        circle.setAttribute('r', 25);
         fresh.classList.add('live');
     }
 
+    fresh.appendChild(circle);
     root.appendChild(fresh);
 
     return fresh;
 }
 
 function updateCell (cell, unit) {
-    cell.setAttribute('cx', unit.posx);
-    cell.setAttribute('cy', unit.posy);
+    if (cell === null) {
+        return;
+    }
+
+    cell.setAttribute('transform', `translate(${unit.posx}, ${unit.posy})`);
 
     if (unit.type === 'Spawner') {
         cell.setAttribute('stroke-dashoffset', ~~(100 - unit._hp_ / 100 * (1000 - 844)));
