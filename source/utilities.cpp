b36685f1b5612c716b4e62ed04207b8aebc27c54
Przemek <przemko924@gmail.com>
2016-12-17 01:34:52 +0100

    randFloatRange added

--- source/utilities.cpp
+++ source/utilities.cpp
@@ -4,3 +4,7 @@
 int randRange(int min, int max) {
   return min + (std::rand() % (max - min + 1));
 }
+
+float randFloatRange(float min, float max) {
+  return randRange(min*1000000.0, max*1000000.0)/1000000.0;
+}
--- source/utilities.hpp
+++ source/utilities.hpp
@@ -1,1 +1,2 @@
 int randRange(int min, int max);
+float randFloatRange(float min, float max);
